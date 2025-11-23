39ab4eac97bb66a23d32240421566ccb71891ed0
kopalajakub40-cyber <kopalajakub40@gmail.com>
2025-11-23 19:06:49 +0000

    zadanie3_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,43 +2,25 @@ import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
-    Scanner sc= new Scanner(System.in);
-    System.out.print("Podaj wyskość choinki:");
-    int height = sc.nextInt();
-    System.out.print("Podaj znak do rysowania choinki:");
-    String znak = sc.next();
-    char ch = znak.charAt(0);
+    Scanner sc = new Scanner(System.in);
 
-    if(height < 1){
-      System.out.print("Wysokość musi być większa lub równa 1");
-      return;
+    System.out.println("Podaj tekst: ");
+    String tekst = sc.nextLine();
+
+    StringBuilder wynik = new StringBuilder();
+
+    for(int i=0;i<tekst.length();i++){
+      char c = tekst.charAt(i);
+
+      if(Character.isUpperCase(c)){
+        wynik.append(Character.toLowerCase(c));
+      } else if(Character.isLowerCase(c)){
+        wynik.append(Character.toUpperCase(c));
+      } else {
+        wynik.append(c);
+      }
     }
 
-    drawTree(height, ch);
-    drawTrunk(height, ch);
-  }
-  private static void drawTree(int height, char  ch){
-    for (int row = 0; row < height; row++){
-      int spaces = height - row - 1;
-      int count = 2 * row + 1;
-
-
-
-      for (int i=0;i<spaces;i++) System.out.print(' ');
-      for (int i=0;i<count;i++) System.out.print(ch);
-      System.out.println();
-    }
-  }
-  private static void drawTrunk(int height, char ch){
-    int trunkWidth = Math.max(1, height / 3);
-    if(trunkWidth % 2 == 0) trunkWidth++;
-    int totalWidth = height * 2 - 1;
-    int spaces =(totalWidth - trunkWidth)/2;
-
-    for(int r=0;r<2;r++){
-      for(int i=0;i<spaces;i++) System.out.print(' ');
-      for(int i=0;i<trunkWidth;i++) System.out.print(ch);
-      System.out.println();
-    }
+    System.out.println("Po zmianie:" +wynik);
   }
 }
