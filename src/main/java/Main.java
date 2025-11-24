432b396871e902c1b759f609f5fa4fc9797a0d0b
kopalajakub40-cyber <kopalajakub40@gmail.com>
2025-11-24 08:15:01 +0000

    zadanie3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,31 @@ import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Podaj wyskość choinki:");
+    int height = sc.nextInt();
+    System.out.print("Podaj znak do rysowania choinki:");
+    String znak = sc.next();
+    char ch = znak.charAt(0);
+    System.out.print("Podaj liczbe kopii: ");
+    int copies = sc.nextInt();
 
-    System.out.println("Podaj tekst: ");
-    String tekst = sc.nextLine();
-
-    StringBuilder wynik = new StringBuilder();
-
-    for(int i=0;i<tekst.length();i++){
-      char c = tekst.charAt(i);
-
-      if(Character.isUpperCase(c)){
-        wynik.append(Character.toLowerCase(c));
-      } else if(Character.isLowerCase(c)){
-        wynik.append(Character.toUpperCase(c));
-      } else {
-        wynik.append(c);
-      }
+    if(height < 1){
+      System.out.print("Wysokość musi być większa lub równa 1");
+      return;
     }
 
-    System.out.println("Po zmianie:" +wynik);
+    for (int i = 0;i<copies;i++){
+      drawTree(height, ch);
+      System.out.println();
+    }
+  }
+  private static void drawTree(int height, char  ch){
+    for (int row = 0; row < height; row++){
+      int count = (row + 1);
+      for (int i=0;i<count;i++) System.out.print(ch);
+      System.out.println();
+    }
+    
   }
 }
